081ead15c00d58bbd68a61a3da2ec1be4303acab
wille <w.sandstrom@gmail.com>
2017-01-22 13:06:44 +0000

    client: retry connection

--- client/main.go
+++ client/main.go
@@ -1,39 +1,49 @@
 package main
 
-import "crypto/tls"
+import (
+	"crypto/tls"
+	"fmt"
+)
 
 func main() {
 	InitPackets()
 
-	service := "localhost:9999"
-
-	conn, err := tls.Dial("tcp", service, &tls.Config{
-		InsecureSkipVerify: true,
-	})
-
-	if err != nil {
-		panic(err)
-	}
-
-	con := Connection{
-		Conn: conn,
-	}
-
-	con.Init()
-
 	for {
-		header, err := con.ReadHeader()
+		host := "localhost:9999"
+		fmt.Println("Connecting to", host)
+
+		conn, err := tls.Dial("tcp", host, &tls.Config{
+			InsecureSkipVerify: true,
+		})
 
 		if err != nil {
-			panic(err)
+			fmt.Println(err.Error())
+			continue
 		}
 
-		packet := GetIncomingPacket(header)
-		err = packet.Read(&con)
+		con := Connection{
+			Conn: conn,
+		}
 
-		if err != nil {
-			panic(err)
+		con.Init()
+
+		for {
+			header, err := con.ReadHeader()
+
+			if err != nil {
+				fmt.Println(err.Error())
+				conn.Close()
+				break
+			}
+
+			packet := GetIncomingPacket(header)
+			err = packet.Read(&con)
+
+			if err != nil {
+				fmt.Println(err.Error())
+				conn.Close()
+				break
+			}
 		}
 	}
-
 }
