f6a18a84d40281bbb551bdca9a444948fb9adc71
wille <w.sandstrom@gmail.com>
2017-01-24 14:52:40 +0000

    macos: screen capture

---
+++ client/screen/screen_darwin.c
@@ -0,0 +1,128 @@
+#import <ApplicationServices/ApplicationServices.h>
+
+#include "bitmap.h"
+#include "screen.h"
+
+CGContextRef CreateARGBBitmapContext(CGImageRef inImage);
+
+void QueryMonitors(void) {
+    CGDisplayCount displayCount;
+    CGDirectDisplayID displays[32];
+
+    CGGetActiveDisplayList(32, displays, &displayCount);
+
+    for (int i = 0; i < displayCount; i++) {
+		CGDirectDisplayID display = displays[i];
+		CGImageRef image = CGDisplayCreateImage(display);
+
+		int width = CGDisplayPixelsWide(display);
+		int height = CGDisplayPixelsHigh(display);
+
+		Monitor m;
+
+		m.id = display;
+		m.coordinates.x = 0;
+		m.coordinates.y = 0;
+		m.coordinates.width = width;
+		m.coordinates.height = height;
+
+		MonitorCallback(m);
+    }
+}
+
+char *GetScreenshot(Monitor monitor, int *size) {
+	CGDisplayCount displayCount;
+    CGDirectDisplayID displays[32];
+    CGGetActiveDisplayList(32, displays, &displayCount);
+
+	CGDirectDisplayID display;
+
+    for (int i = 0; i < displayCount; i++) {
+		CGDirectDisplayID d = displays[i];
+
+		if (d == monitor.id) {
+			display = d;
+			break;
+		}
+	}
+
+	CGImageRef image = CGDisplayCreateImage(display);
+
+    CGContextRef cgctx = CreateARGBBitmapContext(image);
+	if (cgctx == NULL) {
+		return NULL;
+	}
+	
+	size_t w = CGImageGetWidth(image);
+	size_t h = CGImageGetHeight(image);
+	CGRect rect = {{0, 0}, {w, h}};
+
+	CGContextDrawImage(cgctx, rect, image);
+
+	char *buf;
+
+	void *data = CGBitmapContextGetData(cgctx);
+	if (data != NULL) {
+		buf = GetBitmap(data, size, w, h);
+	}
+
+	CGContextRelease(cgctx);
+
+	if (data) {
+		free(data);
+	}
+
+	return buf;
+}
+
+CGContextRef CreateARGBBitmapContext(CGImageRef inImage) {
+    CGContextRef context = NULL;
+    CGColorSpaceRef colorSpace;
+    void *bitmapData;
+    int bitmapByteCount;
+    int bitmapBytesPerRow;
+
+    // Get image width, height. We'll use the entire image.
+    size_t pixelsWide = CGImageGetWidth(inImage);
+    size_t pixelsHigh = CGImageGetHeight(inImage);
+
+    // Declare the number of bytes per row. Each pixel in the bitmap in this
+    // example is represented by 4 bytes; 8 bits each of red, green, blue, and
+    // alpha.
+    bitmapBytesPerRow = (pixelsWide * 4);
+    bitmapByteCount = (bitmapBytesPerRow * pixelsHigh);
+
+    // Use the generic RGB color space.
+    colorSpace = CGColorSpaceCreateWithName(kCGColorSpaceGenericRGB);
+    if (colorSpace == NULL) {
+		fprintf(stderr, "Error allocating color space\n");
+		return NULL;
+    }
+
+    // Allocate memory for image data. This is the destination in memory
+    // where any drawing to the bitmap context will be rendered.
+    bitmapData = malloc(bitmapByteCount);
+    if (bitmapData == NULL) {
+		fprintf(stderr, "Memory not allocated!");
+		CGColorSpaceRelease(colorSpace);
+		return NULL;
+    }
+
+    // Create the bitmap context. We want pre-multiplied ARGB, 8-bits
+    // per component. Regardless of what the source image format is
+    // (CMYK, Grayscale, and so on) it will be converted over to the format
+    // specified here by CGBitmapContextCreate.
+    context = CGBitmapContextCreate(bitmapData, pixelsWide, pixelsHigh, 8, // bits per component
+					bitmapBytesPerRow,
+				    colorSpace,
+				    kCGImageAlphaPremultipliedLast);
+
+    if (context == NULL) {
+		free(bitmapData);
+		fprintf(stderr, "Context not created!");
+    }
+
+    CGColorSpaceRelease(colorSpace);
+
+    return context;
+}
---
+++ client/screen/screen_darwin.go
@@ -0,0 +1,67 @@
+package screen
+
+/*
+#cgo LDFLAGS: -framework ApplicationServices
+
+#include <stdlib.h>
+#include "screen.h"
+#include "bitmap.h"
+*/
+import "C"
+
+import (
+	"bytes"
+	"fmt"
+	"image"
+	"unsafe"
+
+	"github.com/disintegration/imaging"
+
+	"golang.org/x/image/bmp"
+)
+
+// Offset where the bitmap data begins (after file and info header)
+var dataOffset int
+
+func init() {
+	dataOffset = int(C.sizeof_BITMAPFILEHEADER) + int(C.sizeof_BITMAPINFOHEADER)
+}
+
+func Capture(monitor Monitor) image.Image {
+	var m C.Monitor
+
+	m.id = C.int(monitor.id)
+	m.coordinates.x = C.int(monitor.x)
+	m.coordinates.y = C.int(monitor.y)
+	m.coordinates.width = C.int(monitor.width)
+	m.coordinates.height = C.int(monitor.height)
+
+	var len C.int
+
+	buf := C.GetScreenshot(m, &len)
+	buf1 := C.GoBytes(unsafe.Pointer(buf), len)
+
+	for i := dataOffset; i < int(len); i += 4 {
+		r := buf1[i]
+		b := buf1[i+2]
+
+		temp := r
+
+		buf1[i] = b
+		buf1[i+2] = temp
+	}
+
+	img, err := bmp.Decode(bytes.NewReader(buf1))
+
+	C.free(unsafe.Pointer(buf))
+
+	if err != nil {
+		fmt.Println("error decoding bitmap:", err.Error())
+		return nil
+	}
+
+	rotated := imaging.Rotate180(img)
+	rotated = imaging.FlipH(rotated)
+
+	return rotated
+}
