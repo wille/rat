0ce1c0291351293ce28b510da952a6ebeda752cb
wille <w.sandstrom@gmail.com>
2017-01-25 18:48:36 +0000

    screen streaming

--- command/web/screen.ts
+++ command/web/screen.ts
@@ -1,11 +1,20 @@
-function connect(id, elem) {
-	let ws = new WebSocket("wss://localhost:7777/ssock")
+class ScreenStream {
 
-	ws.onmessage = function(event) {
-		elem.src = "data:image/jpg;base64," + event.data;
+	private socket: WebSocket;
+
+	constructor(private id: number, private screen: HTMLImageElement) { }
+
+	public start() {
+		this.socket = new WebSocket("wss://localhost:7777/ssock");
+		this.socket.onmessage = (event) => this.onMessage(event);
+		this.socket.onopen = () => this.onOpen();
 	}
 
-	ws.onopen = function() {
-		ws.send(id + "\n")
+	private onMessage(event) {
+		this.screen.src = "data:image/jpg;base64," + event.data;
+	}
+
+	private onOpen() {
+		this.socket.send(this.id + "\n");
 	}
 }
