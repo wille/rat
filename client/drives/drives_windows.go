a35f863e0c59b11d3b68329c2aaeb7607fde11b7
wille <w.sandstrom@gmail.com>
2017-02-05 17:00:14 +0000

    client: return windows drive letters as uppercase

--- client/drives/drives_windows.go
+++ client/drives/drives_windows.go
@@ -6,10 +6,11 @@ import "os"
 #include "drives.h"
 */
 import "C"
+import "strings"
 
 //export Callback
 func Callback(c C.char) {
-	Drives = append(Drives, DriveInfo{string(c) + ":"})
+	Drives = append(Drives, DriveInfo{strings.ToUpper(string(c)) + ":"})
 }
 
 func QueryDrives() {
