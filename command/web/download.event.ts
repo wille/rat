ff72ad0dfa27d380123af53a9e120ee55318f803
wille <w.sandstrom@gmail.com>
2017-02-06 12:36:24 +0100

    web: redirect to download URL instead since some browsers do not allow a form that is not in the document

--- command/web/download.event.ts
+++ command/web/download.event.ts
@@ -3,15 +3,6 @@
 class DownloadEvent implements Control.IncomingEvent {
 
 	public emit(data) {
-		let form = document.createElement("form");
-		form.setAttribute("action", "/download");
-
-		let file = document.createElement("input");
-		file.setAttribute("type", "hidden");
-		file.setAttribute("name", "key");
-		file.setAttribute("value", data);
-		form.appendChild(file);
-
-		form.submit();
+		document.location.href = "/download?key=" + data;
 	}
 }
