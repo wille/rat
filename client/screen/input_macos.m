5f66683a4ea763cb42b3bac533020d6fb4f76e20
wille <w.sandstrom@gmail.com>
2017-02-07 19:56:09 +0000

    macos: input events

--- client/screen/input_macos.m
+++ client/screen/input_macos.m
@@ -2,23 +2,60 @@
 
 #import <ApplicationServices/ApplicationServices.h>
 
+#include <stdlib.h>
+
 #include "input.h"
 
+// save for click event
+static int lastX, lastY;
+
 void MoveCursor(int m, int x, int y) {
-	CGDisplayCount displayCount;
-    CGDirectDisplayID displays[32];
-    CGGetActiveDisplayList(32, displays, &displayCount);
+	lastX = x;
+	lastY = y;
+	CGDisplayMoveCursorToPoint((CGDirectDisplayID) m, CGPointMake(x, y));
+}
 
-	CGDirectDisplayID display;
-
-    for (int i = 0; i < displayCount; i++) {
-		CGDirectDisplayID d = displays[i];
-
-		if (d == m) {
-			display = d;
-			break;
+void Mouse(int m, int button, int type) {
+	if (button == LEFT) {
+		button = kCGMouseButtonLeft;
+		switch (type) {
+			case PRESS:
+				type = kCGEventLeftMouseDown;
+				break;
+			case RELEASE:
+				type = kCGEventLeftMouseUp;
+				break;
+			default:
+				fprintf(stderr, "invalid event type: %i\n", type);
+				return;
 		}
+	} else if (button == RIGHT) {
+		button = kCGMouseButtonRight;
+		switch (type) {
+			case PRESS:
+				type = kCGEventRightMouseDown;
+				break;
+			case RELEASE:
+				type = kCGEventRightMouseUp;
+				break;
+			default:
+				fprintf(stderr, "invalid event type: %i\n", type);
+				return;
+		}
+	} else {
+		fprintf(stderr, "invalid button type: %i\n", type);
+		return;
 	}
 
-	CGDisplayMoveCursorToPoint(display, CGPointMake(x, y));
+	CGEventRef event = CGEventCreateMouseEvent(NULL, type, CGPointMake(lastX, lastY), button);
+    CGEventPost(kCGHIDEventTap, event);
+	CFRelease(event);
+}
+
+void Key(unsigned short key, int t) {
+	bool keyDown = t == PRESS;
+
+	CGEventRef event = CGEventCreateKeyboardEvent(NULL, (CGKeyCode) key, keyDown);
+	CGEventPost(kCGHIDEventTap, event);
+	CFRelease(event);
 }
