73208f7dd3d8d296ffbfdb2aa1cb2145879f3f82
wille <w.sandstrom@gmail.com>
2017-02-14 10:43:53 +0000

    web: do not set connection status label if it does not exist in the document

--- command/web/connection.ts
+++ command/web/connection.ts
@@ -2,6 +2,10 @@ namespace Connection {
 	export function setConnectionStatus(connected: boolean) {
 		let element = document.getElementById("status");
 
+		if (!element) {
+			return;
+		}
+
 		if (connected) {
 			element.innerHTML = "Connected";
 			element.className = "";
