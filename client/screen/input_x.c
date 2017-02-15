e19de35aa146100bf874fe2b5973abd39546eca3
wille <w.sandstrom@gmail.com>
2017-02-15 15:50:16 +0100

    linux: more input

--- client/screen/input_x.c
+++ client/screen/input_x.c
@@ -2,9 +2,17 @@
 
 #include <X11/Xlib.h>
 
+#include <string.h>
+#include <stdlib.h>
+
 #include "input.h"
 
-void MoveCursor(int m, int x, int y) {
+typedef struct {
+	unsigned short keycode;
+	int type;
+} KeyEvent;
+
+static void Invoke(int m, void *param, void (*func)(Display*, Window, void*)) {
 	Display *display;
     Screen *screen;
     Window root;
@@ -19,11 +27,98 @@ void MoveCursor(int m, int x, int y) {
 		root = RootWindow(display, screen_number);
 		
 		if (screen_number == m) {
-			XSelectInput(display, root, KeyReleaseMask);
-			XWarpPointer(display, None, root, 0, 0, 0, 0, x, y);
+			func(display, root, param);
 			break;
 		}
 	}
 
 	XCloseDisplay(display);
+}
+
+static void moveCursor(Display *display, Window root, void *param) {
+	Coordinates *point = (Coordinates*)param;
+	XSelectInput(display, root, KeyReleaseMask);
+	XWarpPointer(display, None, root, 0, 0, 0, 0, point->x, point->y);
+}
+
+void MoveCursor(int m, int x, int y) {
+	Coordinates p;
+	p.x = x;
+	p.y = y;
+	Invoke(m, &p, &moveCursor);	
+}
+
+static void mouseClick(Display *display, Window root, void *eventptr) {
+	ClickEvent *ce = (ClickEvent*)eventptr;
+	
+	XEvent event;	
+	event.type = ce->type;
+	event.xbutton.button = ce->button;
+	event.xbutton.same_screen = True;
+	
+	if (XSendEvent(display, root, True, 0xfff, &event) == 0) {
+		perror("ERROR");
+	}
+
+	XFlush(display);
+}
+
+void Mouse(int m, int button, int type) {
+	switch (button) {
+		case LEFT:
+			button = Button1;
+			break;
+		case RIGHT:
+			button = Button2;
+			break;
+		case MIDDLE:
+			button = Button3;
+			break;
+	}
+
+	switch (type) {
+		case PRESS:
+			type = ButtonPress;
+			break;
+		case RELEASE:
+			type = ButtonRelease;
+			break;
+	}
+
+	ClickEvent event;
+	event.button = button;
+	event.type = type;
+	Invoke(m, &event, &mouseClick);
+}
+
+static void keyEvent(Display *display, Window root, void *param) {
+	KeyEvent *key = (KeyEvent*) param;
+
+	XEvent ev;
+	ev.type = KeyPress;
+    ev.xkey.state = ShiftMask;
+    ev.xkey.keycode = key->keycode;
+    ev.xkey.same_screen = True;
+
+    if (XSendEvent(display, root, True, KeyPressMask, &ev) == 0) {
+		perror("ERROR");
+	}
+	XFlush(display);
+}
+
+void Key(unsigned short key, int t) {
+	switch (t) {
+		case PRESS:
+			t = KeyPress;
+			break;
+		case RELEASE:
+			t = KeyRelease;
+			break;
+	}
+
+	KeyEvent event;
+	event.keycode = key;
+	event.type = t;
+
+	Invoke(0, &event, &keyEvent);
 }
