271099f3dc0289fabaeed16ec71be06688f8f936
wille <w.sandstrom@gmail.com>
2017-02-18 20:47:49 +0000

    web: create separator when adding dynamic statusbar element

--- command/web/statusbar.ts
+++ command/web/statusbar.ts
@@ -2,11 +2,19 @@ namespace Statusbar {
 
 	let statusbar = <HTMLDivElement>document.getElementById("statusbar");
 
+	function createSeparator() {
+		let element = <HTMLDivElement>document.createElement("div");
+		element.className = "separator";
+		return element;
+	}
+
 	export function addElement(element: HTMLElement) {
+		statusbar.appendChild(createSeparator());
 		statusbar.appendChild(element);
 	}
 
 	export function removeElement(element: HTMLElement) {
 		statusbar.removeChild(element);
+		statusbar.removeChild(statusbar.childNodes[statusbar.childNodes.length - 1]);
 	}
 }
