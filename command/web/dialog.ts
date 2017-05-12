ee6573a7433c6b0cb7f0048ea7a26f6a7b6e4d54
wille <w.sandstrom@gmail.com>
2017-05-12 09:39:05 +0200

    web: dialogs using jQuery UI

---
+++ command/web/dialog.ts
@@ -0,0 +1,61 @@
+/// <reference path="view.ts" />
+
+abstract class Dialog extends AbstractView {
+
+	constructor(url: string, title: string, public dialogButtons: DialogButton[], public id?: number) {
+		super(url, title);
+	}
+
+	abstract onEnter();
+	abstract onLeave();
+}
+
+abstract class DialogButton {
+
+	constructor(public text: string, public close: boolean = true) {
+
+	}
+
+	abstract onClick(parent?: Dialog);
+}
+
+class CancelButton extends DialogButton {
+	
+	constructor() {
+		super("Cancel", true);
+	}
+
+	onClick() {
+		// Will close on click
+	}
+}
+
+function showDialog(dialog: Dialog) {
+	let element = document.createElement("div");
+	element.id = "dialog";
+
+	$(element).load(dialog.url);
+
+	let buttons = {};
+	for (let button of dialog.dialogButtons) {
+		buttons[button.text] = () => {
+			button.onClick(dialog);
+
+			if (button.close) {
+				dialog.onLeave();
+				$(element).dialog("close");
+			}
+		};
+	}
+
+	$(element).dialog({
+		title: dialog.title,
+		resizable: false,
+		height: "auto",
+		width: 400,
+		modal: true,
+		buttons: buttons
+	});
+
+	dialog.onEnter();
+}
