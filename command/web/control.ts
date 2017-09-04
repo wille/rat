b727ee5fd3504eadee55468085ec9d9b5f4154c4
wille <w.sandstrom@gmail.com>
2017-09-04 11:09:54 +0200

    web: fix reference

--- command/web/control.ts
+++ command/web/control.ts
@@ -1,6 +1,7 @@
 /// <reference path="events/downloadProgress.event.ts" />
 /// <reference path="events/client.event.ts" />
 /// <reference path="messages/transfers.message.ts" />
+/// <reference path="events/transfers.event.ts" />
 
 namespace Control {
 
