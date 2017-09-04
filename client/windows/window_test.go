9c5e74133e060bd9e2bc70669f0f283ee9886195
wille <w.sandstrom@gmail.com>
2017-09-04 18:47:16 +0100

    silent window test, check if any title is found

--- client/windows/window_test.go
+++ client/windows/window_test.go
@@ -1,18 +1,27 @@
 package windows
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestQuery(t *testing.T) {
 	QueryWindows()
 
-	fmt.Println("Found", len(Windows), "windows")
+	count := len(Windows)
 
-	for i := 0; i < len(Windows); i++ {
-		w := Windows[i]
+	if count == 0 {
+		t.Error("no windows found")
+	}
 
-		fmt.Println(w.Handle, w.Title, w.Rect)
+	foundTitle := false
+	for _, w := range Windows {
+		if w.Title != "" {
+			foundTitle = true
+			break
+		}
+	}
+
+	if !foundTitle {
+		t.Error("no window was found with a title")
 	}
 }
