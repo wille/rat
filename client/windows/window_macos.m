a1d6169420c96d3ba7f68ca4ea1f52d77962dfea
wille <w.sandstrom@gmail.com>
2017-09-16 16:09:45 +0200

    get title, window handle and bounds properly

--- client/windows/window_macos.m
+++ client/windows/window_macos.m
@@ -7,16 +7,21 @@
 void QueryWindows(void) {
 	CFArrayRef windows = CGWindowListCopyWindowInfo(kCGWindowListExcludeDesktopElements | kCGWindowListOptionOnScreenOnly, kCGNullWindowID);	
 
-	//CFArrayApplyFunction(windows, CFRangeMake(0, CFArrayGetCount(windows)), &ProcessWindow, NULL);
-
 	for (NSDictionary *entry in (NSArray*) windows) {
 		int number = [[entry objectForKey: (id) kCGWindowNumber] intValue];
 		NSString* ownerName = [entry objectForKey: (id) kCGWindowName];
+
+		CGRect bounds;
+        CGRectMakeWithDictionaryRepresentation([entry objectForKey: (id) kCGWindowBounds], &bounds);
 
 		Frame frame;
 		frame.handle = number;
 		frame.visible = false;
 		frame.title = [ownerName UTF8String];
+		frame.rect.x = bounds.origin.x;
+		frame.rect.y = bounds.origin.y;
+		frame.rect.width = bounds.size.width;
+		frame.rect.height = bounds.size.height;
 
 		WindowCallback(frame);
 	}
