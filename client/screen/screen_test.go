72199adc2ac844bdc99c53c7e4ed438bafba3488
wille <w.sandstrom@gmail.com>
2017-09-19 21:23:58 +0200

    capture all windows test

--- client/screen/screen_test.go
+++ client/screen/screen_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"image/jpeg"
 	"os"
+	"rat/client/windows"
 	"strconv"
 	"testing"
 )
@@ -22,6 +23,25 @@ func TestScreenshot(t *testing.T) {
 		jpeg.Encode(file, img, &jpeg.Options{
 			Quality: 75,
 		})
+		file.Close()
+	}
+}
+
+func TestWindows(t *testing.T) {
+	windows.QueryWindows()
+
+	for _, window := range windows.Windows {
+		img := CaptureWindow(window.Handle)
+
+		file, err := os.Create("window" + strconv.Itoa(window.Handle) + ".jpg")
+		if err != nil {
+			t.Error(err)
+		}
+
+		jpeg.Encode(file, img, &jpeg.Options{
+			Quality: 75,
+		})
+
 		file.Close()
 	}
 }
