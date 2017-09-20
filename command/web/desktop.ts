547355d18d1af23c79676950ea3f5468b8074819
wille <w.sandstrom@gmail.com>
2017-09-20 18:46:05 +0200

    web: discard more os element windows

--- command/web/desktop.ts
+++ command/web/desktop.ts
@@ -1,3 +1,5 @@
+/// <reference path="operatingsystem.ts" />
+
 interface Rect {
     x: number;
     y: number;
@@ -10,6 +12,44 @@ interface Frame {
     title: string;
     rect: Rect;
     visible: boolean;
+}
+
+namespace Desktop {
+
+    /**
+     * Disallowed window titles on specific operating systems,
+     * windows that have no meaning or always exists
+     */
+    let titles = [];
+
+    titles[OperatingSystemType.Windows] = [
+        "Program Manager",
+        "Default IME",
+        "MSCTFIME UI"
+    ]
+
+    titles[OperatingSystemType.macOS] = [
+        "Dock - Dock"
+    ]
+
+    /**
+     * Checks if we should care about this frame at all, removes common OS elements etc
+     * @param os operating system type from origin machine of frame
+     * @param frame frame to check
+     */
+    export function displayFrame(os: OperatingSystemType, frame: Frame): boolean {
+        let disallowed = titles[os];
+        
+        if (disallowed) {
+            for (let title of disallowed) {
+                if (title === frame.title) {
+                    return false;
+                }
+            }
+        }
+
+        return frame.title && frame.title.length > 0;
+    }
 }
 
 interface Monitor {
