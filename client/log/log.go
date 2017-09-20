c11c8703e9d9f8cefd691c6d98e6b039f1ad0bf3
wille <w.sandstrom@gmail.com>
2017-09-20 20:10:21 +0200

    add client logging

---
+++ client/log/log.go
@@ -0,0 +1,23 @@
+package log
+
+import (
+	"fmt"
+	"os"
+)
+
+// Errorln prints to stderr
+func Errorln(format string, a ...interface{}) {
+	fmt.Fprintf(os.Stderr, format, a)
+	fmt.Fprintf(os.Stderr, "\n")
+}
+
+// Printf fmt.Printf() wrapper
+func Printf(format string, a ...interface{}) {
+	fmt.Printf(format, a...)
+	fmt.Println()
+}
+
+// Println fmt.Println() wrapper
+func Println(a ...interface{}) {
+	fmt.Println(a...)
+}
