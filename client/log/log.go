c02f0c63319b642c49c96b7ac7deec2109dbc056
wille <w.sandstrom@gmail.com>
2017-09-20 20:12:33 +0200

    disable logging in prod

--- client/log/log.go
+++ client/log/log.go
@@ -1,23 +1,18 @@
+//+build prod
+
 package log
 
-import (
-	"fmt"
-	"os"
-)
-
-// Errorln prints to stderr
+// Errorln does nothing in production
 func Errorln(format string, a ...interface{}) {
-	fmt.Fprintf(os.Stderr, format, a)
-	fmt.Fprintf(os.Stderr, "\n")
+
 }
 
-// Printf fmt.Printf() wrapper
+// Printf does nothing in production
 func Printf(format string, a ...interface{}) {
-	fmt.Printf(format, a...)
-	fmt.Println()
+
 }
 
-// Println fmt.Println() wrapper
+// Println does nothing in production
 func Println(a ...interface{}) {
-	fmt.Println(a...)
+
 }
