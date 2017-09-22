48359b725744bfebb54150928166567cd9d942f9
wille <w.sandstrom@gmail.com>
2017-09-22 16:06:08 +0200

    comments in statusbar

--- command/web/statusbar.ts
+++ command/web/statusbar.ts
@@ -1,18 +1,29 @@
 namespace Statusbar {
 
-	let statusbar = <HTMLDivElement>document.getElementById("statusbar");
+	const statusbar = <HTMLDivElement>document.getElementById("statusbar");
 
+    /**
+     * Creates a status bar separator, should be between each element
+     */
 	function createSeparator() {
 		let element = <HTMLDivElement>document.createElement("div");
 		element.className = "separator";
 		return element;
 	}
 
+    /**
+     * Adds an element to the statusbar
+     * @param element
+     */
 	export function addElement(element: HTMLElement) {
 		statusbar.appendChild(createSeparator());
 		statusbar.appendChild(element);
 	}
 
+    /**
+     * Removes element from statusbar and their separator
+     * @param element 
+     */
 	export function removeElement(element: HTMLElement) {
 		statusbar.removeChild(element);
 		statusbar.removeChild(statusbar.childNodes[statusbar.childNodes.length - 1]);
