b3c1d36016d3252110b7a3cb7ba744920e1da680
wille <w.sandstrom@gmail.com>
2017-09-22 16:09:44 +0200

    move togglebutton element to elements folder

--- command/web/views/elements/togglebutton.ts
+++ command/web/views/elements/togglebutton.ts
@@ -1,12 +1,29 @@
+/**
+ * Toggle button element using Bootstraps button classes
+ */
 class ToggleButton {
 
+    /**
+     * Selected classname (blue)
+     */
     private static readonly SELECTED_CLASSNAME = "btn btn-primary";
+
+    /**
+     * Unselected classname (default, white)
+     */
     private static readonly UNSELECTED_CLASSNAME = "btn btn-default";
 
+    /**
+     * @param element the backing HTML element
+     */
     constructor(private element: HTMLElement) {
 
     }
 
+    /**
+     * Set onclick event, toggle classname and call callback function
+     * @param callback has one argument, boolean checked
+     */
     public set onclick(callback: (checked: boolean) => void) {
         this.element.onclick = () => {
             let selected = this.element.className === ToggleButton.SELECTED_CLASSNAME;
