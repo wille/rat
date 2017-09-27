e65074f07dbf8890218fc0e930f36967f091ad9b
wille <w.sandstrom@gmail.com>
2017-09-27 19:58:00 +0200

    csp header

--- command/webserver.go
+++ command/webserver.go
@@ -9,12 +9,24 @@ import (
 	"strconv"
 )
 
+func setup(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Security-Policy", "script-src 'self'")
+}
+
 func init() {
 	funcMap := template.FuncMap{
 		"Version": func() string { return shared.Version },
 	}
 
+	handler := func(h http.Handler) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			setup(w, r)
+			h.ServeHTTP(w, r)
+		}
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		setup(w, r)
 		t := template.Must(template.New("index.template.html").Funcs(funcMap).ParseFiles("web/index.template.html", "web/head.template.html", "web/tail.template.html"))
 
 		err := t.Execute(w, &Clients)
@@ -24,6 +36,7 @@ func init() {
 		}
 	})
 	http.HandleFunc("/clients", func(w http.ResponseWriter, r *http.Request) {
+		setup(w, r)
 		t := template.Must(template.New("clients.template.html").Funcs(funcMap).ParseFiles("web/clients.template.html"))
 
 		err := t.Execute(w, &Clients)
@@ -33,6 +46,8 @@ func init() {
 	})
 
 	http.HandleFunc("/upload", func(w http.ResponseWriter, r *http.Request) {
+		setup(w, r)
+
 		dir := r.PostFormValue("directory")
 
 		file, header, err := r.FormFile("file")
@@ -53,14 +68,15 @@ func init() {
 		}
 	})
 	http.HandleFunc("/download", func(w http.ResponseWriter, r *http.Request) {
+		setup(w, r)
 		if file, ok := TempFiles[r.FormValue("key")]; ok {
 			w.Header().Set("Content-Disposition", "attachment; filename="+file.Name)
 
 			http.ServeFile(w, r, file.Path)
 		}
 	})
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./web/static"))))
-	http.Handle("/scripts/", http.StripPrefix("/scripts/", http.FileServer(http.Dir("./web/scripts"))))
+	http.Handle("/static/", handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./web/static")))))
+	http.Handle("/scripts/", handler(http.StripPrefix("/scripts/", http.FileServer(http.Dir("./web/scripts")))))
 }
 
 func startWebserver() {
