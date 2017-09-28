4949d68d125096e3f0b272f45ae269f805f21738
wille <w.sandstrom@gmail.com>
2017-09-28 16:14:49 +0200

    web: programatic context menu

--- command/web/contextmenu.ts
+++ command/web/contextmenu.ts
@@ -1,3 +1,123 @@
+abstract class Menu {
+
+    private instance: HTMLElement;
+    
+    constructor(protected text: string) {
+
+    }
+
+    /**
+     * Creates the raw HTML element
+     */
+    protected abstract createElement(): HTMLElement;
+
+    public get element() {
+        if (!this.instance) {
+            this.instance = this.createElement();
+        }
+
+        return this.instance;
+    }
+
+    public set enabled(b: boolean) {
+        this.element.className = b ? "disabled" : "";
+    }
+
+    public get enabled() {
+        return this.instance.className === "disabled";
+    }
+}
+
+/**
+ * A normal single click menu item
+ */
+class MenuItem extends Menu {
+
+    constructor(text: string) {
+        super(text);
+    }
+
+    public createElement() {
+        let li = document.createElement("li");
+
+        let text = document.createElement("a");
+        text.innerText =  this.text;
+        li.appendChild(text);
+
+        li.onclick = () => {
+            this.onclick();
+        }
+
+        return li;
+    }
+
+    public onclick: () => void;
+}
+
+/**
+ * A submenu that holds multiple menuitems
+ */
+class SubMenu extends Menu {
+
+    /**
+     * The HTML element which holds the subitems
+     */
+    private elementsList: HTMLElement;
+
+    constructor(text: string, items?: MenuItem[]) {
+        super(text);
+
+        if (items) {
+            for (let item of items) {
+                this.addItem(item);   
+            }
+        }
+    }
+
+    /**
+     * Add a menu item to this submenu
+     */
+    public addItem(item: MenuItem) {
+        this.element;
+        this.elementsList.appendChild(item.createElement());
+    }
+
+    public addItems(...item: MenuItem[]) {
+        item.forEach((x) => this.addItem(x));
+    }
+
+    public createElement() {
+        // Main submenu element
+        let li = document.createElement("li");
+        li.className = "dropdown-submenu";
+
+        // Main menu element of submenu
+        let a = document.createElement("a");
+        a.innerText = this.text;
+
+        a.onmouseover = () => $(a).next("ul").show();
+		a.onmouseleave = (event: MouseEvent) => {
+			let target = event.toElement;
+
+			if (target.parentElement.parentElement.className !== "dropdown-menu") {
+				$(a).next("ul").hide();
+			}
+		}     
+
+        // Caret icon
+        let caret = document.createElement("span");
+        caret.className = "caret";
+        a.appendChild(caret);
+        li.appendChild(a);
+
+        this.elementsList = document.createElement("ul");
+        this.elementsList.className = "dropdown-menu";
+        li.appendChild(this.elementsList);
+
+        return li;
+    }
+}
+
 abstract class ContextMenu {
 
 	/**
@@ -41,7 +161,11 @@ abstract class ContextMenu {
 
     protected onClose() {
 
-	}
+    }
+    
+    public add(s: Menu) {
+        this.menu.children[0].appendChild(s.element);
+    }
 	
 	/**
 	 * Creates a submenu
