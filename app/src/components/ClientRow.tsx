0cdf3a74003c88d00ed92e550006f31fa66b750c
wille <w.sandstrom@gmail.com>
2017-11-26 14:40:50 +0100

    display ping icon in client row

--- app/src/components/ClientRow.tsx
+++ app/src/components/ClientRow.tsx
@@ -18,7 +18,33 @@ export default class ClientRow extends React.Component<Props, any> {
                 </td>
                 <td>{client.host}</td>
                 <td>{client.identifier}</td>
+                <td>
+                    <img src={"assets/ping/" + this.getPingIcon() + ".png"}/>
+                </td>
             </tr>
         );
     }
+
+    private getPingIcon() {
+        const { client } = this.props;
+        const ms = client.ping;
+        let n;
+
+        if (ms < 100) {
+            n = 0;
+        } else if (ms < 150) {
+            n = 1;
+        } else if (ms < 250) {
+            n = 2;
+        } else if (ms < 350) {
+            n = 3;
+        } else if (ms < 500) {
+            n = 4;
+        } else {
+            n = 5;
+        }
+
+        return "ping" + n;
+    }
+
 }
