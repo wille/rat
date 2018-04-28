0fad3bb42b63f9a3aafcb3e4cf82d64caac36e38
wille <w.sandstrom@gmail.com>
2018-04-28 12:58:38 +0200

    remove react split pane

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,7 @@
 import * as React from 'react';
 import { Nav, NavItem } from 'react-bootstrap';
-import * as SplitPane_ from 'react-split-pane';
 
 import Connections from './components/Clients';
-
-// quick fix for broken typings
-const SplitPane = SplitPane_ as any;
 
 export default class App extends React.Component<any, any> {
 
@@ -17,16 +13,7 @@ export default class App extends React.Component<any, any> {
   public render() {
     const { views, selected } = this.state;
 
-    return (
-      <div>
-        <SplitPane defaultSize='50%' split='vertical'>
-          <Connections client={null} />
-          <div>
-
-          </div>
-        </SplitPane>
-      </div>
-    );
+    return <Connections client={null} />;
   }
 
 }
