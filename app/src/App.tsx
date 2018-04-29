c3272e474d0de80782a7938d1680634bc5ff5026
wille <w.sandstrom@gmail.com>
2018-04-29 13:18:49 +0200

    move routes to App.tsx

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,10 +1,28 @@
 import * as React from 'react';
-import { Nav, NavItem } from 'react-bootstrap';
+import { Route, Switch } from 'react-router';
 
-import Routes from './Routes';
+import Clients from './components/Clients';
+import Card from './components/Clients/Card';
+import FileSystem from './components/FileSystem';
+import Process from './components/Process';
+import Screen from './components/Screen';
 
-export default class App extends React.Component<any, any> {
-  public render() {
-    return <Routes />;
-  }
-}
+const Views = () => (
+  <div>
+    <Card />
+    <Switch>
+      <Route path="/view/screen" component={Screen} />
+      <Route path="/view/process" component={Process} />
+      <Route path="/view/fs" component={FileSystem} />
+    </Switch>
+  </div>
+);
+
+const App = () => (
+  <Switch>
+    <Route path="/" exact component={Clients} />
+    <Route path="/view/" component={Views} />
+  </Switch>
+);
+
+export default App;
