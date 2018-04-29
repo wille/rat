b478c0a60df71ccc94125a8209c98e10006c85db
wille <w.sandstrom@gmail.com>
2018-04-29 13:38:33 +0200

    show loading label if client has not loaded from state

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,27 +1,40 @@
 import * as React from 'react';
 import { Route, Switch } from 'react-router';
 
+import { selectClient } from '@app/reducers';
+import { connect } from 'react-redux';
+
 import Clients from './components/Clients';
 import Card from './components/Clients/Card';
 import FileSystem from './components/FileSystem';
 import Process from './components/Process';
 import Screen from './components/Screen';
 
-const Views = () => (
+const Views = ({ client }) => (
   <div>
-    <Card />
-    <Switch>
-      <Route path="/view/screen" component={Screen} />
-      <Route path="/view/process" component={Process} />
-      <Route path="/view/fs" component={FileSystem} />
-    </Switch>
+    {client ? (
+      <div>
+        <Card />
+        <Switch>
+          <Route path="/view/screen" component={Screen} />
+          <Route path="/view/process" component={Process} />
+          <Route path="/view/fs" component={FileSystem} />
+        </Switch>
+      </div>
+    ) : (
+      <p>Loading</p>
+    )}
   </div>
 );
+
+const Views2 = connect(state => ({
+  client: selectClient(state),
+}))(Views);
 
 const App = () => (
   <Switch>
     <Route path="/" exact component={Clients} />
-    <Route path="/view/" component={Views} />
+    <Route path="/view/" component={Views2} />
   </Switch>
 );
 
