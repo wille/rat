307b2aff9164fb280da710a90b32908b4321e690
wille <w.sandstrom@gmail.com>
2019-04-30 00:18:41 +0200

    app: deps: upgrade react, add css imports
     Please enter the commit message for your changes. Lines starting

--- packages/app/webpack.common.js
+++ packages/app/webpack.common.js
@@ -31,6 +31,10 @@ const config = {
         options: {
           limit: 10000
         }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader','css-loader'],
       }
     ]
   },
