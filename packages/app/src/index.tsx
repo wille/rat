4fb28753cf110fc2e5d2437762176f988c082f7e
wille <w.sandstrom@gmail.com>
2019-05-25 11:24:13 +0200

    app: support for redux devtools

--- packages/app/src/index.tsx
+++ packages/app/src/index.tsx
@@ -14,7 +14,10 @@ import theme from './theme';
 
 ControlSocket.connect();
 
-const store = createStore(reducers);
+const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export default store;
 
