489946c4008bdc993a23e5fdb57280a4e76ae1bb
redpois0n <red@cmail.nu>
2019-09-23 16:19:15 +0200

    remove old code

--- command/old/message_directory_list.go
+++
@@ -1,7 +0,0 @@
-package main
-
-type DirectoryListMessage []File
-
-func (m DirectoryListMessage) Header() MessageHeader {
-	return DirectoryQueryEvent
-}
--- command/old/message_download_query.go
+++
@@ -1,9 +0,0 @@
-package main
-
-type DownloadQueryMessage struct {
-	Key string `json:"key"`
-}
-
-func (m DownloadQueryMessage) Header() MessageHeader {
-	return DownloadQueryHeader
-}
